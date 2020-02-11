f8233bc39055bb4e47f03a20d9ec6aada46e8d6e
JRodDvlpr <Mialienproductions@gmail.com>
2020-02-10 19:16:43 -0500

    MVP

---
+++ rick-morty/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { fetchApiData } from "./actions/";
+import "./App.css";
+import CharacterList from "./components/CharacterList";
+import { connect } from "react-redux";
+import styled from "styled-components";
+
+function App(props) {
+
+  useEffect(() => {
+    props.fetchApiData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header>Rick and Morty Character</Header>
+      <CharacterList />
+    </div>
+  );
+}
+
+export default connect(
+  null,
+  { fetchApiData }
+)(App);
+
+const Header = styled.h1`
+  font-size: 60px;
+  border-bottom: 4px solid black;
+`;
---
+++ rick-morty/src/reducers/index.js
@@ -0,0 +1,30 @@
+
+import {
+    API_FETCH_START,
+    API_FETCH_SUCCESS,
+  } from "../actions";
+  
+
+  const initialState = {
+    cards: [],
+    isFetching: false,
+  };
+
+ 
+  export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case API_FETCH_START:
+        return { 
+          ...state,
+          isFetching: true 
+         };
+         case API_FETCH_SUCCESS:
+         return {
+            ...state,
+            isFetching: false,
+            cards: action.payload
+         }
+      default:
+        return state;
+    }
+  };
